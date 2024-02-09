73f337da7d5d669a8b156eb896fc2d161c6f0459
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2024-02-09 20:23:47 +0800

    new version

--- server.py
+++ server.py
@@ -161,9 +161,9 @@ def create_interface():
     with OpenMonkeyPatch():
         shared.gradio['interface'].launch(
             prevent_thread_lock=True,
-            share=shared.args.share,
-            server_name=None if not shared.args.listen else (shared.args.listen_host or '0.0.0.0'),
-            server_port=shared.args.listen_port,
+            # share=shared.args.share,
+            server_name= '0.0.0.0',
+            # server_port=shared.args.listen_port,
             inbrowser=shared.args.auto_launch,
             auth=auth or None,
             ssl_verify=False if (shared.args.ssl_keyfile or shared.args.ssl_certfile) else True,
